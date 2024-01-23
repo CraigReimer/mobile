f5dfc0fdcbc44a3126e0bcc12c7a2091b90fdddf
Student <student@Mac.local>
2024-01-23 09:45:22 -0800

    ADD:Roman Converter

---
+++ Roman/Roman/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Roman;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void Convert_Clicked(System.Object sender, System.EventArgs e)
+    {
+        int intInput;
+        bool isNumber;
+        Converter converter = new();
+
+        isNumber = Int32.TryParse(txtInput.Text, out intInput);
+
+        if (isNumber)
+        {
+            lblDisplay.Text = converter.NumberToRoman(intInput);
+        }else
+        {
+            lblDisplay.Text = converter.RomanToNumber(txtInput.Text.ToUpper()).ToString();
+        }
+    }
+
+    void Clear_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtInput.Text = string.Empty;
+        lblDisplay.Text = "0";
+    }
+}
+
+
