a1a93017f5fc33f47b004adab3bf0c9d93126e85
Student <student@Mac.local>
2024-02-20 09:28:02 -0800

    ADD: DistanceConverter

---
+++ DistanceConverter/DistanceConverter/Views/StartPage.xaml.cs
@@ -0,0 +1,164 @@
+﻿namespace DistanceConverter.Views;
+
+public partial class StartPage : ContentPage
+{
+    double KeyValue = 0;
+
+    double dbl_m2f = 3.280839895;
+    double dbl_m2i = 39.37007874;
+    double dbl_m2mi = 0.00062137119;
+    double dbl_m2m = 1;
+    double dbl_m2nm = 0.0005399568034557;
+    double dbl_m2y = 1.093613298;
+    double dbl_m2k = 0.001;
+
+    public StartPage()
+    {
+        InitializeComponent();
+        Title = "Distance Converter";
+
+        ToolbarItem tbi = new();
+        tbi.Text = "About";
+        this.ToolbarItems.Add(tbi);
+
+        tbi.Clicked += Tbi_Clicked;
+    }
+
+    private void Tbi_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AboutPage());
+    }
+
+    void btnClear_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtMeters.Text = string.Empty;
+        txtInches.Text = string.Empty;
+        txtFeet.Text = string.Empty;
+        txtYards.Text = string.Empty;
+        txtMiles.Text = string.Empty;
+        txtNautical.Text = string.Empty;
+        txtKilometers.Text = string.Empty;
+    }
+
+    void btnConvert_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtMeters.Text = (KeyValue * dbl_m2m).ToString("g9");
+        txtInches.Text = (KeyValue * dbl_m2i).ToString("g9");
+        txtFeet.Text = (KeyValue * dbl_m2f).ToString("g9");
+        txtYards.Text = (KeyValue * dbl_m2y).ToString("g9");
+        txtMiles.Text = (KeyValue * dbl_m2mi).ToString("g9");
+        txtNautical.Text = (KeyValue * dbl_m2nm).ToString("g9");
+        txtKilometers.Text = (KeyValue * dbl_m2k).ToString("g9");
+    }
+
+    void txtMeters_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        //m2m
+        double dblNumber;
+        bool isValid = Double.TryParse(txtMeters.Text, out dblNumber);
+
+        if (isValid && dblNumber != 0)
+        {
+            KeyValue = dblNumber / dbl_m2m;
+        }
+        {
+            KeyValue = 0;
+        }
+    }
+
+    void txtInches_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        //m2i
+        double dblNumber;
+        bool isValid = Double.TryParse(txtInches.Text, out dblNumber);
+
+        if (isValid && dblNumber != 0)
+        {
+            KeyValue = dblNumber / dbl_m2i;
+        }
+        else
+        {
+            KeyValue = 0;
+        }
+    }
+
+    void txtFeet_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        //m2f
+        double dblNumber;
+        bool isValid = Double.TryParse(txtFeet.Text, out dblNumber);
+
+        if (isValid && dblNumber != 0)
+        {
+            KeyValue = dblNumber / dbl_m2f;
+        }
+        else
+        {
+            KeyValue = 0;
+        }
+    }
+
+    void txtYards_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        //m2y
+        double dblNumber;
+        bool isValid = Double.TryParse(txtYards.Text, out dblNumber);
+
+        if (isValid && dblNumber != 0)
+        {
+            KeyValue = dblNumber / dbl_m2y;
+        }
+        else
+        {
+            KeyValue = 0;
+        }
+    }
+
+    void txtMiles_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        //m2mi
+        double dblNumber;
+        bool isValid = Double.TryParse(txtMiles.Text, out dblNumber);
+
+        if (isValid && dblNumber != 0)
+        {
+            KeyValue = dblNumber / dbl_m2mi;
+        }
+        else
+        {
+            KeyValue = 0;
+        }
+    }
+
+    void txtNautical_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        //m2nm
+        double dblNumber;
+        bool isValid = Double.TryParse(txtNautical.Text, out dblNumber);
+
+        if (isValid && dblNumber != 0)
+        {
+            KeyValue = dblNumber / dbl_m2nm;
+        }
+        else
+        {
+            KeyValue = 0;
+        }
+    }
+
+    void txtKilometers_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        //m2k
+        double dblNumber;
+        bool isValid = Double.TryParse(txtKilometers.Text, out dblNumber);
+
+        if (isValid && dblNumber != 0)
+        {
+            KeyValue = dblNumber / dbl_m2k;
+        }
+        else
+        {
+            KeyValue = 0;
+        }
+    }
+}
---
+++ DistanceConverter/DistanceConverter/obj/Debug/net7.0-android/android/src/crc642d74040f368153b2/MainActivity.java
@@ -0,0 +1,48 @@
+package crc642d74040f368153b2;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("DistanceConverter.MainActivity, DistanceConverter", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("DistanceConverter.MainActivity, DistanceConverter", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("DistanceConverter.MainActivity, DistanceConverter", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
