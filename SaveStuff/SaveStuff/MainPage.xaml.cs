b28e935f5576cc4c80be5ee185fb27f9d24d7c20
Student <student@Mac.local>
2024-02-27 07:14:47 -0800

    Update Stuff

---
+++ SaveStuff/SaveStuff/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace SaveStuff;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void btnSave_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Preferences.Set("UserName", txtUserName.Text);
+    }
+
+    void btnRead_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtUserName.Text = Preferences.Get("UserName", "");
+    }
+
+    void btnClear_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtUserName.Text = string.Empty;
+    }
+}
+
+
