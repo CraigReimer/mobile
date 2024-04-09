54bd3fc2cc8a54de398aaf9f818122e17da34989
Student <student@Mac.local>
2024-04-09 13:09:15 -0700

    Update: RoundedCorners App, GPS App

--- CustomGPS/CustomGPS/MainPage.xaml.cs
+++ CustomGPS/CustomGPS/MainPage.xaml.cs
@@ -29,31 +29,4 @@ public partial class MainPage : ContentPage
         }
 
     }
-
-    private async Task<string> GetGeocodeReverseData(double latitude = 47.673988, double longitude = -122.121513)
-    {
-        IEnumerable<Placemark> placemarks = await Geocoding.Default.GetPlacemarksAsync(latitude, longitude);
-
-        Placemark placemark = placemarks?.FirstOrDefault();
-
-        if (placemark != null)
-        {
-            return
-                $"AdminArea:       {placemark.AdminArea}\n" +
-                $"CountryCode:     {placemark.CountryCode}\n" +
-                $"CountryName:     {placemark.CountryName}\n" +
-                $"FeatureName:     {placemark.FeatureName}\n" +
-                $"Locality:        {placemark.Locality}\n" +
-                $"PostalCode:      {placemark.PostalCode}\n" +
-                $"SubAdminArea:    {placemark.SubAdminArea}\n" +
-                $"SubLocality:     {placemark.SubLocality}\n" +
-                $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
-                $"Thoroughfare:    {placemark.Thoroughfare}\n";
-
-        }
-
-        return "";
-    }
 }
-
-
