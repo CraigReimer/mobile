7986a68105afa700be0e2b45ec58995694739776
Student <student@Mac.local>
2024-04-30 08:57:21 -0700

    MODIFY: List control, swipe to delete, pull to refresh

--- ShoppingList/ShoppingList/Views/MainPage.xaml.cs
+++ ShoppingList/ShoppingList/Views/MainPage.xaml.cs
@@ -7,12 +7,19 @@ public partial class MainPage : ContentPage
 {
     LoginPage lp = new();
 
-	public MainPage()
-	{
-		InitializeComponent();
+    public MainPage()
+    {
+        InitializeComponent();
         this.Loaded += MainPage_Loaded;
         lp.Unloaded += Lp_Unloaded;
-	}
+        lstData.Refreshing += LstData_Refreshing;
+    }
+
+    private void LstData_Refreshing(object sender, EventArgs e)
+    {
+        LoadData();
+        lstData.IsRefreshing = false;
+    }
 
     private void Lp_Unloaded(object sender, EventArgs e)
     {
@@ -34,7 +41,7 @@ public partial class MainPage : ContentPage
         else
         {
             // Do stuff
-            txtInput.Text = App.UserKey;
+            LoadData();
         }
     }
 
@@ -52,5 +59,69 @@ public partial class MainPage : ContentPage
         App.UserKey = "";
 
         OnAppearing1();
+    }
+
+    async void btnAddData_Clicked(System.Object sender, System.EventArgs e)
+    {
+        // Add Item
+        HttpClient client = new();
+
+        var data = JsonConvert.SerializeObject(new UserData(null, txtInput.Text, App.UserKey));
+
+        await client.PostAsync(new Uri("https://joewetzel.com/fvtc/account/data"),
+            new StringContent(data, System.Text.Encoding.UTF8, "application/json"));
+
+        txtInput.Text = "";
+
+        LoadData();
+    }
+
+    async public void LoadData()
+    {
+        // Load Items from API
+        HttpClient client = new();
+
+        var response = await client.GetAsync(new Uri("https://joewetzel.com/fvtc/account/data/" + App.UserKey));
+
+        var wsData = response.Content.ReadAsStringAsync().Result;
+
+        var data = JsonConvert.DeserializeObject<UserDataCollection>(wsData);
+
+        lstData.ItemsSource = data.UserDataItems;
+
+        txtInput.Focus();
+    }
+
+    void DeleteItem_Clicked(System.Object sender, System.EventArgs e)
+    {
+        var mi = (MenuItem)sender;
+        var dataId = mi.CommandParameter.ToString();
+
+        // Delete item from API
+        DeleteData(dataId);
+    }
+
+    async public void DeleteData(string dataId = null)
+    {
+        // Delete item(s) from API
+        HttpClient client = new();
+
+        var data = JsonConvert.SerializeObject(new UserData(dataId, null, App.UserKey));
+
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Delete,
+            RequestUri = new Uri("https://joewetzel.com/fvtc/account/data"),
+            Content = new StringContent(data, System.Text.Encoding.UTF8, "application/json"),
+        };
+
+        await client.SendAsync(request);
+
+        LoadData();
+    }
+
+    void btnClearList_Clicked(System.Object sender, System.EventArgs e)
+    {
+        DeleteData();
     }
 }
